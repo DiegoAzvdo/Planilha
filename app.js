d3cbeb79c105adb3ba7c675d9ddd9518c2cdc4ea
DiegoAC <Diegoazevedocolares2@gmail.com>
2022-08-24 18:13:04 +0000

    sistem

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
+
+const TransacoesRepositorio = require("./transacoes-repositorio")
 const app = express()
 
-const port = 3000
+const port = 3000;
 
 app.use(express.static(`${__dirname}/public`))
 
 app.get('/transacoes', (req, res) => {
-    res.send("Ola mundo!")
-});
+    const repositorio = new TransacoesRepositorio()
+    const transacoes = repositorido.listarTransacoes()
+
+    res.send(transacoes)
+})
 
 app.listen(port, () => {
     console.log(`servidor ouvindo na porta ${port}`);
---
+++ transacoes-repositorio.js
@@ -0,0 +1,25 @@
+class TransacoesRepositorio {
+    transacoes = {
+        transacoes: [
+            {
+                valor: 10,
+                descricao: "Pastel"
+            },
+            {
+                valor: 3,
+                descricao: "doce"
+            }
+        ]
+    }
+
+    listarTransacoes() {
+        return this.transacoes
+    }
+
+    listarTransacoes() {
+        return transacoes
+    }
+}
+
+
+module.exports = TransacoesRepositorio
